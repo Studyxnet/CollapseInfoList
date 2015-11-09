98ab7b7e6a64959785f2392d4b4a1c14c6223ea0
Rafael Moura <contato@rm.eti.br>
2015-11-09 19:47:53 -0200

    First

---
+++ ListCollapsed.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DetailList
+{
+	public partial class ListCollapsed : ContentPage
+	{
+		public ListCollapsed ()
+		{
+			InitializeComponent ();
+
+			BindingContext = new List<Person> () {
+				new Person () {
+					Name = "Rafael",
+					Surname = "Moura",
+					Age = 21,
+					Visible = false
+				},
+				new Person () {
+					Name = "Leonardo",
+					Surname = "Lima",
+					Age = 20,
+					Visible = false
+				},
+				new Person () {
+					Name = "Albert",
+					Surname = "Tanure",
+					Age = 28,
+					Visible = false
+				}
+			};
+		}
+
+		void SelectedList (object sender, SelectedItemChangedEventArgs e)
+		{
+			if (e.SelectedItem != null) {
+				(e.SelectedItem as Person).ChangeVisible ();
+
+
+			}
+
+			(sender as ListView).SelectedItem = null;
+		}
+	}
+}
+
---
+++ Values.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DetailList
+{
+	public class Person : INotifyPropertyChanged
+	{
+		#region INotifyPropertyChanged implementation
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		void OnPropertyChanged (string Prop)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged (this, new PropertyChangedEventArgs (Prop));
+		}
+
+		#endregion
+
+		public Person ()
+		{
+		}
+
+		private string _name;
+
+		public String Name {
+			get { 
+				return _name;
+			}
+			set { 
+				if (_name != value) {
+					_name = value;
+					OnPropertyChanged ("Name");
+				}
+			}
+		}
+
+		private string _surname;
+
+		public String Surname {
+			get { 
+				return _surname;
+			}
+			set { 
+				if (_surname != value) {
+					_surname = value;
+					OnPropertyChanged ("Surname");
+				}
+			}
+		}
+
+		private int _age;
+
+		public int Age {
+			get { 
+				return _age;
+			}
+			set { 
+				if (_age != value) {
+					_age = value;
+					OnPropertyChanged ("Age");
+				}
+			}
+		}
+
+		private bool _visible;
+
+		public bool Visible {
+			get { 
+				return _visible;
+			}
+			set { 
+				if (_visible != value) {
+					_visible = value;
+					OnPropertyChanged ("Visible");
+				}
+			}
+		}
+
+		public void ChangeVisible ()
+		{
+			Visible = !Visible;
+		}
+	}
+}
+
